68f1135c2f567742cc28066a986ae61161044f4b
leonreusch <leon@reusch-home.de>
2026-04-29 23:37:12 +0200

    fix: rename test package

--- src/test/kotlin/de/stammtischHub/terminPilot/TerminPilotApplicationTests.kt
+++ src/test/kotlin/de/stammtischHub/terminPilot/TerminPilotApplicationTests.kt
@@ -1,7 +1,7 @@
-package de.StammtischHub.TerminPilot
+package de.stammtischHub.terminPilot
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
 
 @SpringBootTest
 class TerminPilotApplicationTests {
