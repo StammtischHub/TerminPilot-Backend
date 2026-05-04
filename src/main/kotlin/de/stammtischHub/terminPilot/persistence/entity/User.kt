65539660cce95eb0a75b600d4e99d260d174a543
leonreusch <leon@reusch-home.de>
2026-05-04 18:35:09 +0200

    feat: update setup and resources

--- src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/User.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/User.kt
@@ -1,11 +1,14 @@
 package de.stammtischHub.terminPilot.persistence.entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
 class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
 }
--- src/main/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepository.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepository.kt
@@ -1,8 +1,9 @@
 package de.stammtischHub.terminPilot.persistence.repository
 
+import de.stammtischHub.terminPilot.persistence.entity.User
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository {
-
+    fun findById(id: Long): List<User>
 }
