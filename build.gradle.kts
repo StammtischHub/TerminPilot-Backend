4b9d04faaa8d8d7cc225fdd19e1a9715c4c9d20d
leonreusch <leon@reusch-home.de>
2026-05-10 22:39:43 +0200

    refactor: add dto

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
+    kotlin("plugin.jpa") version "2.2.21"
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
 
