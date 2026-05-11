fbd1ce4595521111fdcdf00775fd1366c6e1cd63
leonreusch <leon@reusch-home.de>
2026-05-11 11:13:28 +0200

    chore: update namings

--- src/test/kotlin/de/stammtischHub/terminPilot/TerminPilotApplicationTests.kt
+++ src/test/kotlin/de/stammtischHub/terminPilot/TerminPilotApplicationTests.kt
@@ -1,12 +1,15 @@
-package de.StammtischHub.TerminPilot
+package de.stammtischHub.terminPilot
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled
 class TerminPilotApplicationTests {
 
 	@Test
+	@Disabled
 	fun contextLoads() {
 	}
 
