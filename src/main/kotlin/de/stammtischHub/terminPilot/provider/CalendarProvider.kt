562cc1477e40f86ecb474278841a15cdb56a3903
SirDaywalker <J_Dickel@web.de>
2026-05-12 11:23:37 +0200

    feat: setup first methods

--- src/main/kotlin/de/stammtischHub/terminPilot/provider/CalendarProvider.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/provider/CalendarProvider.kt
@@ -1,3 +1,16 @@
 package de.stammtischHub.terminPilot.provider
 
-interface CalendarProvider
+import java.time.LocalDateTime
+import de.stammtischHub.terminPilot.domain.Appointment
+
+interface CalendarProvider {
+    /**
+     * Gets the calendar-entries from a user for the given timespan.
+     */
+    fun getCalenderForTimespan(start: LocalDateTime, end: LocalDateTime): List<Appointment>
+
+    /**
+     * Writes the given appointment to the users calendar.
+     */
+    fun writeToCalendar(appointment: Appointment)
+}
