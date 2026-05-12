1aeb4f99f2da2225696788c1185495ba2ba5a9ff
SirDaywalker <J_Dickel@web.de>
2026-05-12 11:24:52 +0200

    feat: add Appointment Domain data class

---
+++ src/main/kotlin/de/stammtischHub/terminPilot/domain/Appointment.kt
@@ -0,0 +1,11 @@
+package de.stammtischHub.terminPilot.domain
+
+import java.time.LocalDateTime
+
+data class Appointment(
+    val title: String,
+    val start: LocalDateTime,
+    val end: LocalDateTime,
+    val location: String,
+    val description: String,
+)
