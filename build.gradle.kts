ab0983b76ba8cafcd5ae32e40497a2e836724409
leonreusch <leon@reusch-home.de>
2026-05-14 16:25:19 +0200

    chore(deps): update plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-    id("io.gitlab.arturbosch.detekt") version "1.23.6"
-    id("org.owasp.dependencycheck") version "9.2.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.owasp.dependencycheck") version "12.2.2"
 }
 
 ktlint {
