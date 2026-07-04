3a24757ecb3916ae36948a356f8fb48ee14651d1
leonreusch <leon@reusch-home.de>
2026-07-04 14:15:20 +0200

    feat: update controller to use api spec

--- src/main/kotlin/de/stammtischHub/terminPilot/api/CalendarController.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/api/CalendarController.kt
@@ -1,6 +1,9 @@
 package de.stammtischHub.terminPilot.api
 
+import de.stammtischHub.terminPilot.api.generated.CalendarApi
+import de.stammtischHub.terminPilot.model.generated.CalendarEvent
 import de.stammtischHub.terminPilot.provider.google.GoogleCalendarService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,21 +14,23 @@ import java.time.LocalDateTime
 @RequestMapping("/api/calendar")
 class CalendarController(
   private val googleCalendarService: GoogleCalendarService,
-) {
+) : CalendarApi {
   @GetMapping("/events")
-  fun getEvents(
+  override fun getEvents(
     @RequestParam userId: Long,
-  ): List<Map<String, String>> {
+  ): ResponseEntity<List<CalendarEvent>> {
     val now = LocalDateTime.now()
     val end = now.plusMonths(1)
-    return googleCalendarService.getCalendarForTimespan(userId, now, end).map { appointment ->
-      mapOf(
-        "title" to appointment.title,
-        "start" to appointment.start.toString(),
-        "end" to appointment.end.toString(),
-        "location" to appointment.location,
-        "description" to appointment.description,
-      )
-    }
+    val events =
+      googleCalendarService.getCalendarForTimespan(userId, now, end).map { appointment ->
+        CalendarEvent(
+          title = appointment.title,
+          start = appointment.start.toString(),
+          end = appointment.end.toString(),
+          location = appointment.location,
+          description = appointment.description,
+        )
+      }
+    return ResponseEntity.ok(events)
   }
 }
