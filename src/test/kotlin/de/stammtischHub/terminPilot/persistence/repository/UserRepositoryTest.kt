ec6cc8531625a8615dd973ad62af8d78cecac9a4
Leon Reusch <leon@reusch-home.de>
2026-07-14 21:57:57 +0200

    test: add new user repository tests

--- src/test/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepositoryTest.kt
+++ src/test/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepositoryTest.kt
@@ -4,12 +4,12 @@ import de.stammtischHub.terminPilot.persistence.entity.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager
+import org.springframework.boot.jpa.test.autoconfigure.find
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @DataJpaTest
 class UserRepositoryTest {
-
   @Autowired
   lateinit var entityManager: TestEntityManager
 
@@ -18,12 +18,38 @@ class UserRepositoryTest {
 
   @Test
   fun `should save user`() {
-    val user = User(username = "test", password = "password")
-    entityManager.persist(user)
+    val user = userRepository.save(User(username = "test", password = "password"))
     entityManager.flush()
+    entityManager.clear()
+
+    val foundUser = entityManager.find<User>(user.id!!)!!
+
+    assertEquals(user, foundUser)
+  }
+
+  @Test
+  fun `should update existing user`() {
+    val user = entityManager.persist(User(username = "test", password = "password"))
+    entityManager.flush()
+    entityManager.clear()
+
+    val persistedUser = entityManager.find<User>(user.id!!)!!
+    persistedUser.password = "newPassword"
+    userRepository.save(persistedUser)
+    entityManager.flush()
+    entityManager.clear()
+
+    val updatedUser = entityManager.find<User>(user.id!!)!!
+    assertEquals("newPassword", updatedUser.password)
+  }
+
+  @Test
+  fun `should find user by id`() {
+    val user = entityManager.persist(User(username = "test", password = "password"))
+    entityManager.flush()
+    entityManager.clear()
 
     val foundUser = userRepository.findById(user.id!!).get()
     assertEquals(user, foundUser)
   }
-
 }
