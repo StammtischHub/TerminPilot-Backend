b7402924c0aa4cfeef9012521fe8262697d90b3c
Leon Reusch <leon@reusch-home.de>
2026-07-17 16:06:46 +0200

    test: update UserRepositoryTest

--- src/test/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepositoryTest.kt
+++ src/test/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserRepositoryTest.kt
@@ -1,12 +1,14 @@
 package de.stammtischHub.terminPilot.persistence.repository
 
 import de.stammtischHub.terminPilot.persistence.entity.User
+import de.stammtischHub.terminPilot.persistence.entity.UserType
+import jakarta.validation.ConstraintViolationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager
-import org.springframework.boot.jpa.test.autoconfigure.find
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 @DataJpaTest
 class UserRepositoryTest {
@@ -17,39 +19,36 @@ class UserRepositoryTest {
   lateinit var userRepository: UserRepository
 
   @Test
-  fun `should save user`() {
-    val user = userRepository.save(User(username = "test", password = "password"))
+  fun `should find user by username`() {
+    val user = userRepository.save(User().apply {
+      username = "username"
+      password = "password"
+      userType = UserType.USER
+    })
     entityManager.flush()
     entityManager.clear()
 
-    val foundUser = entityManager.find<User>(user.id!!)!!
-
+    val foundUser = userRepository.findByUsername("username")
     assertEquals(user, foundUser)
   }
 
   @Test
-  fun `should update existing user`() {
-    val user = entityManager.persist(User(username = "test", password = "password"))
-    entityManager.flush()
-    entityManager.clear()
-
-    val persistedUser = entityManager.find<User>(user.id!!)!!
-    persistedUser.password = "newPassword"
-    userRepository.save(persistedUser)
-    entityManager.flush()
-    entityManager.clear()
-
-    val updatedUser = entityManager.find<User>(user.id!!)!!
-    assertEquals("newPassword", updatedUser.password)
+  fun `should throw exception when creating a user with blank fields`() {
+    assertFailsWith<ConstraintViolationException> {
+      userRepository.save(User().apply {
+        username = ""
+        password = ""
+        userType = UserType.USER
+      })
+      entityManager.flush()
+    }
   }
 
   @Test
-  fun `should find user by id`() {
-    val user = entityManager.persist(User(username = "test", password = "password"))
-    entityManager.flush()
-    entityManager.clear()
-
-    val foundUser = userRepository.findById(user.id!!).get()
-    assertEquals(user, foundUser)
+  fun `should throw exception when creating a user with null fields`() {
+    assertFailsWith<ConstraintViolationException> {
+      userRepository.save(User())
+      entityManager.flush()
+    }
   }
 }
