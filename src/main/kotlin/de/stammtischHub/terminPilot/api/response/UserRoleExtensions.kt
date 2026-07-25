d3697e489a0fde036475c0969f61e7ffa2958f34
leonreusch <leon@reusch-home.de>
2026-07-25 16:05:41 +0200

    refactor: extract extension functions

---
+++ src/main/kotlin/de/stammtischHub/terminPilot/api/response/UserRoleExtensions.kt
@@ -0,0 +1,17 @@
+package de.stammtischHub.terminPilot.api.response
+
+import de.stammtischHub.terminPilot.model.generated.UserRole
+import de.stammtischHub.terminPilot.persistence.entity.UserType
+
+fun String.toUserRole(): UserRole? =
+  when (this) {
+    "ADMIN" -> UserRole.admin
+    "USER" -> UserRole.user
+    else -> null
+  }
+
+fun UserType.toUserRole(): UserRole? =
+  when (this) {
+    UserType.ADMIN -> UserRole.admin
+    UserType.USER -> UserRole.user
+  }
