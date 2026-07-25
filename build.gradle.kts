8feaad66b7aaf95ea8647eaceb5e259494714960
leonreusch <leon@reusch-home.de>
2026-07-25 16:05:56 +0200

    feat: add user endpoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
   implementation("io.swagger.core.v3:swagger-annotations:2.2.53")
   implementation("io.swagger.core.v3:swagger-models:2.2.53")
   implementation("jakarta.validation:jakarta.validation-api")
-  apiSpec("de.stammtischhub:terminpilot-api-spec:1.0.1@yaml")
+  apiSpec("de.stammtischhub:terminpilot-api-spec:1.1.0-pr.37.2627660@yaml")
 
   // Provider
   implementation("com.github.lookfirst:sardine:5.13")
---
+++ src/main/kotlin/de/stammtischHub/terminPilot/exception/UserNotFoundException.kt
@@ -0,0 +1,3 @@
+package de.stammtischHub.terminPilot.exception
+
+class UserNotFoundException : RuntimeException("User could not be found")
