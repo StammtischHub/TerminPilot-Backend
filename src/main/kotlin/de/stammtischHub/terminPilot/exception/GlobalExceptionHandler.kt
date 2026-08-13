df1efb772cee524f9cdb765318a07f7d8ac49738
SirDaywalker <J_Dickel@web.de>
2026-08-13 10:20:50 +0200

    fix: remove double declaration of UserNotFound-Handling

--- src/main/kotlin/de/stammtischHub/terminPilot/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/exception/GlobalExceptionHandler.kt
@@ -41,10 +41,6 @@ class GlobalExceptionHandler {
     return ResponseEntity.status(404).body(ex.message)
   }
 
-  @ExceptionHandler(UserNotFoundException::class)
-  fun handleUserNotFound(ex: UserNotFoundException): ResponseEntity<String> =
-    ResponseEntity.status(404).body(ex.message)
-
   @ExceptionHandler(AuthenticationException::class)
   fun onAuthenticationFailure(): ProblemDetail {
     logger.info("Authentication failure")
