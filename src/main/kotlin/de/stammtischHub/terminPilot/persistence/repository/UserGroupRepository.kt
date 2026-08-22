8eef2614296362bd52f9e38e5eeb8a35711ff01f
Leon Reusch <leon@reusch-home.de>
2026-08-22 15:33:10 +0200

    feat!: update user group entity with creator field
    # Conflicts:
    #	src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/User.kt
    #	src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/UserGroup.kt
    #	src/main/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserGroupRepository.kt
    #	src/test/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserGroupRepositoryTest.kt

--- src/main/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserGroupRepository.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/persistence/repository/UserGroupRepository.kt
@@ -1,5 +1,6 @@
 package de.stammtischHub.terminPilot.persistence.repository
 
+import de.stammtischHub.terminPilot.persistence.entity.User
 import de.stammtischHub.terminPilot.persistence.entity.UserGroup
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
@@ -8,6 +9,5 @@ import java.util.Optional
 @Repository
 interface UserGroupRepository : JpaRepository<UserGroup, Long> {
   fun findByName(name: String): Optional<UserGroup>
-
   fun findByCreatorId(creatorId: Long): Optional<Iterable<UserGroup>>
 }
