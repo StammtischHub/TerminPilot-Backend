16384b61c9f44abdc8ef0800dacd38697c46ddf6
Leon Reusch <leon@reusch-home.de>
2026-09-22 18:35:46 +0200

    feat: update constraint and column names

--- src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/UserGroup.kt
+++ src/main/kotlin/de/stammtischHub/terminPilot/persistence/entity/UserGroup.kt
@@ -11,24 +11,30 @@ import jakarta.persistence.Table
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
+import jakarta.persistence.UniqueConstraint
 
 @Entity(name = "user_group")
-@Table(name = "user_groups")
+@Table(
+  name = "user_groups",
+  uniqueConstraints = [
+    UniqueConstraint(name = "unique_name_per_creator", columnNames = ["name", "creator"])
+  ]
+)
 @AttributeOverride(name = "_id", column = Column(name = "user_group_id"))
 class UserGroup : BaseLongId() {
   @NotBlank
   lateinit var name: String
 
   @ManyToOne
-  @JoinColumn(name = "creator_user_id")
+  @JoinColumn(name = "creator")
   @NotNull
   var creator: User? = null
 
   @ManyToMany
   @JoinTable(
-    name = "userGroupMembers",
-    joinColumns = [JoinColumn(name = "user_group_id")],
-    inverseJoinColumns = [JoinColumn(name = "member_user_id")],
+    name = "members",
+    joinColumns = [JoinColumn(name = "user_group")],
+    inverseJoinColumns = [JoinColumn(name = "member")],
   )
   @NotEmpty
   var members: MutableSet<User> = mutableSetOf()
